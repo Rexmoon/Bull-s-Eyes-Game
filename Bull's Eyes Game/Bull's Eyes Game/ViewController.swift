676e4bd9188d4bf6ce95b525fe8f8ad1cc92219f
User-P2 <>
2023-04-12 12:13:28 -0700

    First Button of my App

--- Bull's Eyes Game/Bull's Eyes Game/ViewController.swift
+++ Bull's Eyes Game/Bull's Eyes Game/ViewController.swift
@@ -9,10 +9,22 @@ import UIKit
 
 final class ViewController: UIViewController {
 
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
+    
+    // MARK: - IBActions
+    @IBAction private func showAlert() {
+        let alertController = UIAlertController(title: "Controller Title",
+                                      message: "Controller Message",
+                                      preferredStyle: .alert)
+        
+        let alertAction = UIAlertAction(title: "Action Title",
+                                        style: .cancel,
+                                        handler: nil)
+        
+        alertController.addAction(alertAction)
+        present(alertController, animated: true, completion: nil)
+    }
 }
-
