10cca48a37b0ca2edf34e32fba74336199daa1e0
User-P2 <>
2023-04-14 16:30:59 -0700

    feat: IBOulets references

--- Bull's Eyes Game/Bull's Eyes Game/ViewController.swift
+++ Bull's Eyes Game/Bull's Eyes Game/ViewController.swift
@@ -8,17 +8,27 @@
 import UIKit
 
 final class ViewController: UIViewController {
+    // MARK: - IBOtlets
+    @IBOutlet private var slider: UISlider!
+    @IBOutlet private var targetLabel: UILabel!
+    
     // MARK: - Properties
-    private var currentSliderValue: Int = 50
+    private var currentSliderValue: Int = 0
+    private var targetValue: Int = 0
 
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        startNewRound()
     }
     
     // MARK: - IBActions
     @IBAction private func showAlert() {
-        let message = "The Slider value is \(currentSliderValue)"
+        let message = """
+            The value of slider is \(currentSliderValue)
+            The target value os: \(targetValue)
+        """
+        
         let alertController = UIAlertController(title: "Success",
                                       message: message,
                                       preferredStyle: .alert)
@@ -29,9 +39,22 @@ final class ViewController: UIViewController {
         
         alertController.addAction(alertAction)
         present(alertController, animated: true, completion: nil)
+        startNewRound()
     }
     
     @IBAction private func movedSlider(_ slider: UISlider) {
         currentSliderValue = lroundf(slider.value)
+    }
+    
+    // MARK: - Functions
+    private func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentSliderValue = 50
+        slider.value = Float(currentSliderValue)
+        updateLabels()
+    }
+    
+    private func updateLabels() {
+        targetLabel.text = "\(targetValue)"
     }
 }
